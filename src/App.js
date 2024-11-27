cb6933b27878e8bb2eaf25b7001014ca6e51ba6a
Andriy Trofimov <webgrandmo@gmail.com>
2024-11-27 19:08:11 +0200

    Add context

--- src/App.js
+++ src/App.js
@@ -4,34 +4,37 @@ import Footer from './components/layout/Footer';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
+import { GithubProvider } from './components/context/github/GithubContext';
 function App() {
 	return (
-		<Router>
-			<div className='flex flex-col justify-between h-screen'>
-				<Navbar title='Github Finder' />
-				<main className='container mx-auto px-3 pb-13'>
-					<Routes>
-						<Route
-							path='/'
-							element={<Home />}
-						/>
-						<Route
-							path='/about'
-							element={<About />}
-						/>
-						<Route
-							path='/nontfound'
-							element={<NotFound />}
-						/>
-						<Route
-							path='/*'
-							element={<NotFound />}
-						/>
-					</Routes>
-				</main>
-				<Footer />
-			</div>
-		</Router>
+		<GithubProvider>
+			<Router>
+				<div className='flex flex-col justify-between h-screen'>
+					<Navbar title='Github Finder' />
+					<main className='container mx-auto px-3 pb-13'>
+						<Routes>
+							<Route
+								path='/'
+								element={<Home />}
+							/>
+							<Route
+								path='/about'
+								element={<About />}
+							/>
+							<Route
+								path='/nontfound'
+								element={<NotFound />}
+							/>
+							<Route
+								path='/*'
+								element={<NotFound />}
+							/>
+						</Routes>
+					</main>
+					<Footer />
+				</div>
+			</Router>
+		</GithubProvider>
 	);
 }
 
