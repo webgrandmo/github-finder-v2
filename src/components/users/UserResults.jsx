3829b016f6c0fe6293114f21b64561f4eb88d514
Andriy Trofimov <webgrandmo@gmail.com>
2024-11-27 19:08:06 +0200

    User results

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,31 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+import GithubContext from '../context/github/GithubContext';
 
 function UserResults() {
-	const [users, setUsers] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+	const { users, isLoading, fetchUsers } = useContext(GithubContext);
 	useEffect(() => {
 		fetchUsers();
 	}, []);
 
-	const fetchUsers = async () => {
-		const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-			headers: {
-				Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-			},
-		});
-		const data = await response.json();
-		console.log(data);
-
-		setUsers(data);
-	};
-	return (
-		<div>
-			User Results
-			{users.map((user) => {
-				return <p>{user.login}</p>;
-			})}
-		</div>
-	);
+	if (!isLoading) {
+		return (
+			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+				{users.map((user) => {
+					return (
+						<UserItem
+							key={user.id}
+							user={user}
+						/>
+					);
+				})}
+			</div>
+		);
+	} else {
+		return <Spinner />;
+	}
 }
 
 export default UserResults;
