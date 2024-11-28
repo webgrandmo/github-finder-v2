6905e6798ccf89c6523d25d147fdecb9ea0e44cf
Andriy Trofimov <webgrandmo@gmail.com>
2024-11-28 19:06:34 +0200

    Add reducers

--- src/components/context/github/GithubContext.js
+++ src/components/context/github/GithubContext.js
@@ -1,21 +1,33 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
-	const [users, setUsers] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
-
-	const fetchUsers = async () => {
-		const response = await fetch(`${GITHUB_URL}/users`);
-		const data = await response.json();
-		setUsers(data);
-		setIsLoading(false);
+	const initialState = {
+		users: [],
+		isLoading: false,
 	};
 
-	return <GithubContext.Provider value={{ users, isLoading, fetchUsers }}>{children}</GithubContext.Provider>;
+	const [state, dispatch] = useReducer(githubReducer, initialState);
+	const fetchUsers = async () => {
+		setLoading();
+		const response = await fetch(`${GITHUB_URL}/users`);
+		const data = await response.json();
+		dispatch({
+			type: 'GET_USERS',
+			payload: data,
+		});
+	};
+
+	const setLoading = () => {
+		dispatch({
+			type: 'SET_LOADING',
+		});
+	};
+	return <GithubContext.Provider value={{ users: state.users, isLoading: state.isLoading, fetchUsers }}>{children}</GithubContext.Provider>;
 };
 
 export default GithubContext;
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,14 +1,10 @@
-import { useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
 import GithubContext from '../context/github/GithubContext';
 
 function UserResults() {
-	const { users, isLoading, fetchUsers } = useContext(GithubContext);
-	useEffect(() => {
-		fetchUsers();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	const { users, isLoading } = useContext(GithubContext);
 
 	if (!isLoading) {
 		return (
